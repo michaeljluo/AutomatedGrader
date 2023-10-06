226f5a1a1492a31499e98800f2610d586bea8c72
Michael Luo <mjl826@nyu.edu>
2023-10-05 21:57:07 -0400

    bash 1 working

--- homework01/1.bat
+++ homework01/1.bat
@@ -1,6 +1,6 @@
 @echo off
 
-findstr readfileSync '..\..\%2\%2-%1\bin\game.mjs'
+findstr readfileSync "..\..\%2\%2-%1\bin\game.mjs"
 
 start "Terminal 1" cmd /k "echo 'Terminal 1: Command 1' && cd "..\..\%2\%2-%1" && npm install"
 start "Terminal 2" cmd /k "echo 'Terminal 2: Command 2' && cd "..\..\%2\%2-%1" && git log"
@@ -8,5 +8,5 @@ start "Terminal 3" cmd /k "echo 'Terminal 3: Command 3' && cd "..\..\%2\%2-%1" &
 start "Terminal 4" cmd /k "echo 'Terminal 4: Command 4' && cd "..\..\%2\%2-%1" && more package.json"
 echo '../../%2/%2-%1'
 
-copy mjl_test1.json "..\..\%2\%2-%1\test"
-copy mjl_test2.json "..\..\%2\%2-%1\test"
+copy mjl_test1.json "..\..\%2\%2-%1\test" > NUL
+copy mjl_test2.json "..\..\%2\%2-%1\test" > NUL
