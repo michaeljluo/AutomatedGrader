f95fe690da6b582e95caef1f4b3d798260eb1633
Michael Luo <mjl826@nyu.edu>
2023-10-13 18:22:16 -0400

    refactored first bash script

--- homework02/homework02.py
+++ homework02/homework02.py
@@ -3,9 +3,9 @@ import sys
 from ahk import AHK
 
 #example:
-#python3 homework02.py 2 mjl826 0
+#python3 homework02.py homework02 mjl826 1
 
-netid = sys.argv[2]
+gitId = sys.argv[2]
 hwNum = sys.argv[1]
 if sys.argv[3] == '0':
     ahk = AHK(executable_path="C:/Program Files/AutoHotkey/AutoHotkeyU64.exe")
@@ -21,13 +21,13 @@ if sys.argv[3] == 'q':
     ahk.run_script(ahk_script, blocking=False)
 if sys.argv[3] == '1':
     batch_script = "1.bat"
-    subprocess.Popen([batch_script, netid, hwNum], shell=True)
+    subprocess.Popen([batch_script, gitId, hwNum], shell=True)
 if sys.argv[3] == '2':
     batch_script = "2.bat"
-    subprocess.Popen([batch_script, netid, hwNum], shell=True)
+    subprocess.Popen([batch_script, gitId, hwNum], shell=True)
 if sys.argv[3] == '3':
     batch_script = "3.bat"
-    subprocess.Popen([batch_script, netid, hwNum], shell=True)
+    subprocess.Popen([batch_script, gitId, hwNum], shell=True)
 if sys.argv[3] == '4':
     batch_script = "4.bat"
-    subprocess.Popen([batch_script, netid, hwNum], shell=True)
+    subprocess.Popen([batch_script, gitId, hwNum], shell=True)
