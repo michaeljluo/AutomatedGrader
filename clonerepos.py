2ad1071127c1bb8822e27a61aee09c3c04d8eda0
Michael Luo <mjl826@nyu.edu>
2023-10-13 17:37:53 -0400

    added example command for cloning repos

--- clonerepos.py
+++ clonerepos.py
@@ -1,15 +1,18 @@
 import os
 import subprocess
+import sys
+
+#example: python3 clonerepos.py https://github.com/nyu-csci-ua-0467-001-002-fall-2023/ homework02
 
 # Path to the file containing GitHub usernames
-usernames_file = '../github_usernames.txt'
+usernames_file = 'github_usernames.txt'
 
 # Directory where you want to clone the repositories
 clone_directory = '../' + sys.argv[2]
 
 # Function to clone a GitHub repository
 def clone_repo(username, repo_name):
-    repo_url = sys.argv[1] + f'{repo_name}.git'
+    repo_url = f'{sys.argv[1]}{repo_name}.git'
     clone_path = os.path.join(clone_directory, repo_name)
 
     # Use subprocess to run the 'git clone' command
@@ -28,4 +31,4 @@ with open(usernames_file, 'r') as file:
 
 # Iterate through each username and clone their repositories
 for username in usernames:
-    clone_repo(username, + sys.argv[2]+"-"+username)
+    clone_repo(username,str(sys.argv[2]) +"-"+username)
