7001ac373da1065b88e2c2f46946b24aaab1ddb2
Michael Luo <mjl826@nyu.edu>
2023-10-14 16:48:20 -0400

    refactored 2nd and 3rd bash file

--- homework02/2.bat
+++ homework02/2.bat
@@ -3,7 +3,8 @@
 start "Terminal 1" cmd /k "echo 'Terminal 1: Command 1' && cd "..\..\%2\%2-%1" && npx eslint src/*"
 start "Terminal 2" cmd /k "echo 'Terminal 2: Command 2' && cd "..\..\%2\%2-%1" && npx mocha tests/hoffy-test.mjs"
 start "Terminal 3" cmd /k "echo 'Terminal 3: Command 3' && cd "..\..\%2\%2-%1" && node src/drawing.mjs"
-start "Terminal 4" cmd /k "echo 'Terminal 4: Command 4' && cd "..\..\%2\%2-%1" && node src/server.mjs"
+"C:\Program Files\Notepad++\notepad++.exe" ..\..\%2\%2-%1\src\drawing.mjs
+
 
 
 start "" "file:\\\C:\Users\mjl82\AIT Grading\%2\%2-%1\test.svg"
--- homework02/3.bat
+++ homework02/3.bat
@@ -1,4 +1,4 @@
 @echo off
 
-"C:\Program Files\Notepad++\notepad++.exe" ..\..\%2\%2-%1\src\drawing.mjs
-"C:\Program Files\Notepad++\notepad++.exe" ..\..\%2\%2-%1\src\server.mjs
+start "Terminal 1" cmd /k "cd "..\..\%2\%2-%1" && more src\server.mjs"
+start "Terminal 2" cmd /k "cd "..\..\%2\%2-%1" && node src/server.mjs"
