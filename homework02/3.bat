67b28662317845640d99c64fd0809d9d5418ae7d
Michael Luo <mjl826@nyu.edu>
2023-10-14 16:04:50 -0400

    refactored 2nd bash script

--- homework02/3.bat
+++ homework02/3.bat
@@ -1,6 +1,4 @@
 @echo off
 
-start "Terminal 1" cmd /k "echo 'Terminal 1: Command 1' && cd "..\..\%2\%2-%1" && pause && node bin/game.mjs test/mjl_test2.json"
-start "Terminal 2" cmd /k "echo 'Terminal 2: Command 2' && cd "..\..\%2\%2-%1" && pause && node bin/game.mjs test/test-01-draw-and-find-an-8.json"
-start "Terminal 3" cmd /k "echo 'Terminal 3: Command 3' && cd "..\..\%2\%2-%1" && pause && node bin/game.mjs"
-start "Terminal 4" cmd /k "echo 'Terminal 4: Command 4' && cd cd "..\..\%2\%2-%1" && pause && node bin/game.mjs"
+"C:\Program Files\Notepad++\notepad++.exe" %2\%1-%1\src\drawing.mjs
+"C:\Program Files\Notepad++\notepad++.exe" %2\%1-%1\src\server.mjs
