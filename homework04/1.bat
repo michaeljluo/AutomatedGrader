04ca96ec982754b1a60c46a7e17d868f7fb2238d
Michael Luo <mjl826@nyu.edu>
2023-11-21 22:40:03 -0500

    updated hw4 script

--- homework04/1.bat
+++ homework04/1.bat
@@ -4,4 +4,5 @@ start "Terminal 1" cmd /k "cd "..\..\%2\%2-%1" && npm install"
 
 start "Terminal 2" cmd /k "cd "..\..\%2\%2-%1" && echo "need package.json, dependencies, .gitignore, ESLINT CONFIG" && dir && more package.json"
 start "Terminal 3" cmd /k "cd "..\..\%2\%2-%1" && git log"
+copy .env "..\..\%2\%2-%1" > NUL
 
--- homework04/2.bat
+++ homework04/2.bat
@@ -2,7 +2,7 @@
 start "Terminal 1" cmd /k "cd "..\..\%2\%2-%1" && node app.mjs"
 start "Terminal 2" cmd /k "cd "..\..\%2\%2-%1" && npx eslint db.mjs && npx eslint app.mjs"
 
-start "Terminal 3" cmd /k "cd "..\..\%2\%2-%1" && echo "courseNumber, courseName, semester, year, professor, review" && more db.mjs"
-start "Terminal 4" cmd /k "findstr express.static "..\..\%2\%2-%1\app.mjs" && findstr req.query "..\..\%2\%2-%1\app.mjs""
+start "Terminal 3" cmd /k "cd "..\..\%2\%2-%1" && echo "needs courseNumber, courseName, semester, year, professor, review" && more db.mjs"
+start "Terminal 4" cmd /k "echo "need express.static and req.query" && findstr express.static "..\..\%2\%2-%1\app.mjs" && findstr req.query "..\..\%2\%2-%1\app.mjs""
 
 
