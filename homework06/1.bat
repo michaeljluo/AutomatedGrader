18d34b47d17b4d2897c4e036481e3f8382f71aa5
Michael Luo <mjl826@nyu.edu>
2023-12-18 02:10:14 -0500

    hw6 script changes

--- homework06/1.bat
+++ homework06/1.bat
@@ -1,10 +1,5 @@
 @echo off
-
-start "Terminal 1" cmd /k "cd "..\..\%2\%2-%1" && npm install"
-
-start "Terminal 2" cmd /k "cd "..\..\%2\%2-%1" && echo "need package.json and .gitignore" && dir && more package.json"
-start "Terminal 3" cmd /k "cd "..\..\%2\%2-%1\src" && echo "need package.json and .gitignore" && dir"
-
-start "Terminal 4" cmd /k "cd "..\..\%2\%2-%1" && git log"
+start "Terminal 1" cmd /k "cd "..\..\%2\%2-%1" && node src/app.mjs"
+start "Terminal 2" cmd /k "cd "..\..\%2\%2-%1" && git log"
 
 copy .env "..\..\%2\%2-%1" > NUL
--- homework06/2.bat
+++ homework06/2.bat
@@ -1,7 +1,5 @@
 @echo off
 start "Terminal 1" cmd /k "cd "..\..\%2\%2-%1" && node src/app.mjs"
-start "Terminal 2" cmd /k "cd "..\..\%2\%2-%1" && npx eslint --fix src/**"
-
 
 
 
